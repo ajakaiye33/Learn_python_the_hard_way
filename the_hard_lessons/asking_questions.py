fd33deb0f90babd772ea5ddd68a3b19be9b3f2f9
Hedgar <Hedgar@Orisha-iMac.local>
2018-12-01 19:14:25 -0800

    making little sense

---
+++ the_hard_lessons/asking_questions.py
@@ -0,0 +1,21 @@
+print("How old are you", end=' ')
+age = input()
+
+print("How tall are you?", end=' ')
+height = input()
+
+print("How much do you weigh?", end=' ')
+weight = input()
+
+#print(f"So you're {age} old, {height} tall and {weight} heavy.")
+
+
+# drills
+
+college = input("what college did you attend?")
+
+university = input("what university did you go?")
+
+duration_uni = input("what year did you finish university?")
+
+print(f"So  you're {age} old, {height} tall and {weight} heavy, you went to {college.title()} college, {university.title()} university, where you graduated in {duration_uni}" )
