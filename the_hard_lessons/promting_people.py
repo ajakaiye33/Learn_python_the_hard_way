a60b464dda9109954977d7427b966f515e125371
Hedgar <Hedgar@Orisha-iMac.local>
2018-12-02 20:12:28 -0800

    corrected

--- the_hard_lessons/promting_people.py
+++ the_hard_lessons/promting_people.py
@@ -3,4 +3,4 @@ height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
 
 
-print("So, you're {age} old, {height} tall and {weight} heavy.")
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
