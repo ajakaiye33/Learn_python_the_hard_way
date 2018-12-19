b77d97a4251c79405f6ab60160b83bbaa57ed8b8
Hedgar <ajakzhedgar@gmail.com>
2018-12-19 04:52:11 +0100

    the antology od if statements

---
+++ the_hard_lessons/ex29.py
@@ -0,0 +1,45 @@
+people = 20
+
+cats = 30
+
+dogs = 15
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+
+if people > cats:
+    print("Not too many cats! The world is saved!")
+
+if people < dogs:
+    print("The world is drooled on!")
+
+if people > dogs:
+    print("The world is dry!")
+
+dogs += 5
+
+if people >= dogs:
+    print("People are greater than or equal to dogs.")
+
+if people <= dogs:
+    print("People are less than or equal to dogs.")
+
+
+if people == dogs:
+    print("People are dogs")
+
+peole += 5
+
+
+#drills
+# if statement, essentially check truthy of the condition statement, when
+# true run the code under it.
+# the code under the if statment is indented four spaces beacause it should be unique
+# and if it is not indented it make draw up an error
+
+if people != dogs:
+    print("Hey , Humans are superior!")
+
+
+#if the variable people, dogs and cats are uttered for any reason it will automatically
+# affect the if- statement output
