859d61f921e3f5cbda5511c1e0277d96c17ac53e
Hedgar <ajakzhedgar@gmail.com>
2018-12-21 05:59:37 +0100

    decision

---
+++ the_hard_lessons/ex31.py
@@ -0,0 +1,79 @@
+# print("""You enter a dark room with two doors.
+# Do you go through door #1 or door #2""")
+#
+# door = input("> ")
+#
+#
+# if door == "1":
+#     print("There's a giant bear here eating a cheese cake.")
+#     print("What do you do?")
+#     print("1 . Take the cake.")
+#     print("2. Scream at the bear.")
+#
+#
+#
+#     bear = input("> ")
+#
+#
+#     if bear == "1":
+#         print("The bear eats your face off. Good job!")
+#     elif bear == "2":
+#         print("The bear eats your leg off. Good job!")
+#     else:
+#         print(f"Well, doing {bear} is probably better.")
+#         print("Bear runs away.")
+#
+# elif door == "2":
+#     print("You stare into the endless abyss at Cthul's retina.")
+#     print("1. Blueberrings.")
+#     print("2. Yellow jacket clothespins.")
+#     print("3. Understanding revolvers yelling melodies")
+#
+#
+#     insanity = input("> ")
+#
+#
+#     if insanity == "1" or insanity == "2":
+#         print("Your body survives powered by a mind of jello.")
+#         print("Good job!")
+#
+#     else:
+#
+#         print("The insanity rots your eyes into a pool of muck.")
+#         print("Good job!")
+#
+#
+# else:
+#     print("You stumble around and fall on a knife and die. Good job!")
+
+#drill
+
+print(""" Welcome to the game: Dilemmma
+ You are in a boat with your beatiful bouncing
+ baby boy on a vacation. Then there was a storm threatening
+ to kill everyone of you unless one person is thrown off the boat.
+ who do you sacrifice? #1 Yourself #2 your mother #3 your wife #4 your beautiful
+ daughter""")
+
+save = input("> ")
+if save == '1':
+     print("You drawn and you family saved")
+
+     print("Now you are at the gate before judgement of your god")
+     print("1. suiciders goes to hell")
+     print("2. stay in purgatory with the option to help other on earth")
+
+     heaven_hell = input("> ")
+
+     if heaven_hell == "1":
+         print("You enter eternal doom of suffering.")
+     elif heaven_hell == '2':
+         print("You become a crusada for your god")
+     else:
+         print('You remain without judgement')
+
+elif save == "2":
+    print("you are scared for life")
+
+else:
+    print("You are not allowed t be on the fence")
