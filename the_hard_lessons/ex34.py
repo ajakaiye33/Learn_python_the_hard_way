99f14cdbb3dd6f92ca6d8345e1f67a31535972e9
Hedgar <ajakzhedgar@gmail.com>
2018-12-24 05:23:17 +0100

    indexing from another perspective

---
+++ the_hard_lessons/ex34.py
@@ -0,0 +1,36 @@
+# practing location of items in a list
+#ordinal and cardinal
+
+
+
+animals = ['bear', 'python3.6','peakcock', 'kangaroo', 'whale', 'platypus']
+
+#The animal at 1
+
+print("The animal at 1 is the second animal and is a python3.6 ",animals[1]=='python3.6')
+
+#The third animal
+
+print("The third animal is at 2 and is peakcock", animals[2]== 'peakcock')
+
+# animal at 3
+
+print("The animal at 3 is the fourth animal and is kangaroo", animals[3]=='kangaroo')
+
+# the first (1st) animal
+print("The first animal is at 0 and is a bear", animals[0] == 'bear')
+
+# The fifth(5th) animal
+
+print("The fifth animal is at 4 and is a whale", animals[4]== 'whale')
+
+# The animal at 2
+print("The animal at 2 is the third animal and is peakcock", animals[2]== 'peakcock')
+
+# The sixth (6th) animal
+
+print("The sixth animal is at 5 and is a platypus", animals[5]== 'platypus')
+
+# The animal at 4
+
+print(" The animal at 4 is the fifth animal and is whale",animals[4]== 'whale')
