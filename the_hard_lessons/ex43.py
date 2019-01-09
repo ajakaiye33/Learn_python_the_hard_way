f2fa671b160f05c1e65598ab28f7aff28e39ab2f
Hedgar <ajakzhedgar@gmail.com>
2019-01-09 06:25:45 +0100

    made it work

--- the_hard_lessons/ex43.py
+++ the_hard_lessons/ex43.py
@@ -24,18 +24,20 @@ class Engine(object):
 
         while current_scene != last_scene:
             next_scene_name = current_scene.enter()
-            current_scene = self.scene_map.next_scene_name(next_scene_name)
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
             # be sure to print to print out the last scene
+        current_scene.enter()
 
 class Death(Scene):
 
     quips = [
-            "You died. You kinda suck at this"
-            "Your Mom would be proud ... if she were smarter"
-            "Such a luser"
-            "I have a small puppy that better ay this"
-            "You 're worse your Dad's jokes"
-    ]
+            "You died. You kinda suck at this.",
+            "Your Mom would be proud ... if she were smarter.",
+            "Such a luser.",
+            "I have a small puppy that better at this.",
+            "You 're worse than your Dad's jokes."
+            ]
 
     def enter(self):
         print(Death.quips[randint(0, len(self.quips)-1)])
@@ -44,8 +46,7 @@ class Death(Scene):
 class CentralCorridor(Scene):
 
     def enter(self):
-        def enter(self):
-            print(dedent("""
+        print(dedent("""
                   The Gothons of Planet Percal #25 have invaded your ship and
                  destroyed your entire crew.  You are the last surviving
                  member and your last mission is to get the neutron destruct
@@ -60,13 +61,13 @@ class CentralCorridor(Scene):
                  filled body.  He's blocking the door to the Armory and
                  about to pull a weapon to blast you.
 
-
-
                  """))
+
         action = input("> ")
 
         if action == "shoot!":
             print(dedent("""
+
                       Quick on the draw you yank out your blaster and fire
                       it at the Gothon.  His clown costume is flowing and
                       moving around his body, which throws off your aim.
@@ -81,6 +82,7 @@ class CentralCorridor(Scene):
 
         elif action == "dodge!":
             print(dedent("""
+
                       Like a world class boxer you dodge, weave, slip and
                       slide right as the Gothon's blaster cranks a laser
                       past your head.  In the middle of your artful dodge
@@ -88,9 +90,7 @@ class CentralCorridor(Scene):
                       wall and pass out.  You wake up shortly after only to
                       die as the Gothon stomps on your head and eats you.
 
-
-
-                  """))
+                      """))
             return 'death'
 
         elif action == "tell a joke":
@@ -140,33 +140,129 @@ class LaserWeaponArmory(Scene):
             if guess == code:
                 print(dedent("""
 
-                  The container clicks open and the seal breaks, letting
-                  gas out.  You grab the neutron bomb and run as fast as
-                  you can to the bridge where you must place it in the
-                  right spot.
-                  """))
+                      The container clicks open and the seal breaks, letting
+                      gas out.  You grab the neutron bomb and run as fast as
+                      you can to the bridge where you must place it in the
+                      right spot.
+                      """))
+                return 'the_bridge'
+
+            else:
+                print(dedent("""
+
+                      The lock buzzes one last time and then you hear a
+                      sickening melting sound as the mechanism is fused
+                      together.  You decide to sit there, and finally the
+                      Gothons blow up the ship from their ship and you die.
+                      """))
+                return 'death'
+
+
 
 class TheBridge(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+
+              You burst onto the Bridge with the netron destruct bomb
+              under your arm and surprise 5 Gothons who are trying to
+              take control of the ship.  Each of them has an even uglier
+              clown costume than the last.  They haven't pulled their
+              weapons out yet, as they see the active bomb under your
+              arm and don't want to set it off.
+
+            """))
+        action = input(" >")
+
+        if action == "throw the bomb":
+            print(dedent("""
+
+                  In a panic you throw the bomb at the group of Gothons
+                  and make a leap for the door.  Right as you drop it a
+                  Gothon shoots you right in the back killing you.  As
+                  you die you see another Gothon frantically try to
+                  disarm the bomb. You die knowing they will probably
+                  blow up when it goes off.
+
+
+                """))
+            return 'escape_pod'
+        else:
+            print("DOES NOT COMPUTE!")
+            return "the_bridge"
 
 class EscapePod(Scene):
 
     def enter(self):
-        pass
+        print(dedent("""
+
+              You rush through the ship desperately trying to make it to
+              the escape pod before the whole ship explodes.  It seems
+              like hardly any Gothons are on the ship, so your run is
+              clear of interference.  You get to the chamber with the
+              escape pods, and now need to pick one to take.  Some of
+              them could be damaged but you don't have time to look.
+              There's 5 pods, which one do you take?
+              """))
+        good_pod = randint(1,5)
+
+        guess = input("[pod #]> ")
+
+
+        if int(guess) != good_pod:
+            print(dedent("""
+
+                  You jump into pod {guess} and hit the eject button.
+                  The pod escapes out into the void of space, then
+                  implodes as the hull ruptures, crushing your body into
+                  jam jelly.
+                  """))
+            return 'death'
+
+        else:
+            print(dedent("""
+
+                  You jump into pod {guess} and hit the eject button.
+                  The pod easily slides out into space heading to the
+                  planet below.  As it flies to the planet, you look
+                  back and see your ship implode then explode like a
+                  bright star, taking out the Gothon ship at the same
+                  time.  You won!
+
+                  """))
+            return 'finished'
+
+class Finished(Scene):
+
+    def enter(self):
+        print("You won! Good job.")
+        return 'finished'
 
 
 class Map(object):
 
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        'finished': Finished(),
+
+    }
+
     def __init__(self, start_scene):
-        pass
+
+        self.start_scene = start_scene
+
+
 
     def next_scene(self, scene_name):
-        pass
+        val = Map.scenes.get(scene_name)
+        return val
 
     def opening_scene(self):
-        pass
+        return self.next_scene(self.start_scene)
 
 
 a_map = Map('central_corridor')
