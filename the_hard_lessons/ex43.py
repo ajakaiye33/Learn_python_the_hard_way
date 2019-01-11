e2f944ac91f519baeaa58b046db3bcae6c0682c8
Hedgar <ajakzhedgar@gmail.com>
2019-01-11 05:22:26 +0100

    closing

--- the_hard_lessons/ex43.py
+++ the_hard_lessons/ex43.py
@@ -22,6 +22,7 @@ class Engine(object):
         current_scene = self.scene_map.opening_scene()
         last_scene = self.scene_map.next_scene('finished')
 
+
         while current_scene != last_scene:
             next_scene_name = current_scene.enter()
             current_scene = self.scene_map.next_scene(next_scene_name)
